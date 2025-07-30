ef46d0668c9a738914b7da9c3791dacfbfd6c349
zlay0701 <906105783@qq.com>
2025-07-30 16:12:30 +0800

    优化配置

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,10 +91,7 @@ const config: Config = {
           title: '社交媒体',
           items: [
             { label: '关于我', to: '/about' },
-            { label: 'GitHub', href: social.github.href },
-            { label: 'Twitter', href: social.x.href },
-            { label: '掘金', href: social.juejin.href },
-            { label: 'Discord', href: social.discord.href },
+            { label: 'GitHub', href: 'https://github.com/zlay0701' },
           ],
         },
         {
