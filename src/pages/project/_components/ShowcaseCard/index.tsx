f5877375b4da6de389f7cc40d0a77ca1c2fe225a
zlay0701 <906105783@qq.com>
2025-08-01 16:00:00 +0800

    优化项目主页样式

--- src/pages/project/_components/ShowcaseCard/index.tsx
+++ src/pages/project/_components/ShowcaseCard/index.tsx
@@ -42,11 +42,22 @@ const ShowcaseCard = memo(({ project }: { project: Project }) => {
   return (
     <MagicCard key={project.title} className={cn('card', styles.showcaseCard)}>
       {project.preview && (
-        <div className={cn('card__image', styles.showcaseCardImage)}>
-          <Image src={project.preview} alt={project.title} img={project.preview} />
+        <div className={cn('card__image', styles.showcaseCardImage)}
+          style={{
+            borderBottom: `0px solid var(--ifm-color-emphasis-200)`,
+          }}>
+          <Image src={project.preview} alt={project.title} img={project.preview} 
+            style={{
+              WebkitMaskImage: 'linear-gradient(180deg, #fff -17.19%, #00000000 92.43%)',
+              maskImage: 'linear-gradient(180deg, #fff -17.19%, #00000000 92.43%)',
+            }}/>
         </div>
       )}
-      <div className="card__body">
+      <div className="card__body"
+        style={{
+          backdropFilter: `blur(1px)`,
+          marginTop: `${project.preview ? '-80px' : '0px'}`,
+        }}>
         <div className={cn(styles.showcaseCardHeader)}>
           <h4 className={styles.showcaseCardTitle}>
             <Link href={project.website}>{project.title}</Link>
@@ -63,7 +74,10 @@ const ShowcaseCard = memo(({ project }: { project: Project }) => {
         </div>
         <p className={styles.showcaseCardBody}>{project.description}</p>
       </div>
-      <ul className={cn('card__footer', styles.cardFooter)}>
+      <ul className={cn('card__footer', styles.cardFooter)}
+        style={{
+          backdropFilter: `blur(1px)`,
+        }}>
         <ShowcaseCardTag tags={project.tags} />
       </ul>
     </MagicCard>
