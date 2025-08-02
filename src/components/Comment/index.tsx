5d2e74e29cce03aac41ef4250ed45ae900864df4
zlay0701 <906105783@qq.com>
2025-08-02 20:45:40 +0800

    优化评论服务

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -3,17 +3,38 @@ import type { ThemeConfig } from '@docusaurus/preset-classic'
 import { useColorMode, useThemeConfig } from '@docusaurus/theme-common'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import GiscusComment from '@site/src/components/Comment/Giscus'
+import UtterancesComments from '@site/src/components/Comment/Utterances'
+import Waline from '@site/src/components/Comment/Waline'
 import type { GiscusConfig } from '@site/src/components/Comment/Giscus'
-// frontMatter 可能不存在
+/**
+ * frontMatter 可能不存在 增加不展示评论校验
+ * 支持格式
+comments: false
+
+comment:
+  enable: false
+
+comment: false
+
+hide_comment: true
+ */
 export default function Comment({ frontMatter }: { frontMatter: { [key: string]: unknown } }): JSX.Element {
   const themeConfig = useThemeConfig() as ThemeConfig & { giscus: GiscusConfig }
   const { i18n } = useDocusaurusContext()
   console.log('themeConfig', frontMatter)
   console.log('i18n', i18n)
   const commentsArr = themeConfig.comments
+  const utterancesProps = themeConfig.utterances
+  // 增加不展示校验
   for (const str of commentsArr) {
     if (str.toLowerCase() === 'Giscus'.toLowerCase()) {
       return <GiscusComment />
+    }
+    if (str.toLowerCase() === 'utterances'.toLowerCase()) {
+      return <UtterancesComments repo={utterancesProps.repo} issueTerm={utterancesProps.issueTerm} theme={utterancesProps.theme} />
+    }
+    if (str.toLowerCase() === 'waline'.toLowerCase()) {
+      return <Waline />
     }
   }
 }
